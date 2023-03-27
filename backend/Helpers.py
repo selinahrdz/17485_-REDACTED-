cfdb88c408defeced97a44598319b64f85d2ba6e
Alexis Torres <alexistorres@utexas.edu>
2023-03-26 22:44:03 -0500

    Defined App Routes, Helper Funtion for tasks

---
+++ backend/Helpers.py
@@ -0,0 +1,164 @@
+from pymongo import MongoClient
+from bson import ObjectId
+import json, jsonEncoder, certifi
+
+
+client = MongoClient("mongodb+srv://test:test@cluster0.xylfgq2.mongodb.net/?retryWrites=true&w=majority")
+db = client.Management
+user_collection = db.Users
+project_collection = db.Projects
+hw_set_collection = db.HWSet
+
+
+# USER VALIDATION
+def encrypt(passw):
+    encrypt = ""
+    for char in passw:
+        newC = char
+        if ord(newC) - 13 < 34:
+            newC = chr(127 - (34 - (ord(newC) - 13)))
+        else:
+            newC = chr(ord(char) - 13)
+        encrypt += newC
+    return encrypt[::-1]
+
+
+def decrypt(passw):
+    decrypt = ""
+    for char in passw:
+        newC = char
+        if ord(newC) + 13 > 126:
+            newC = chr(33 + ord(newC) - 126 + 13)
+        else:
+            newC = chr(ord(char) + 13)
+        decrypt += newC
+    return decrypt[::-1]
+
+def sign_in(username, password): #Returns Json
+    user = user_collection.find_one({'Username':username})
+    if user is not None:
+        account_password = decrypt(user['Password'])
+        if password == account_password:
+            return {'message' : 'Authorized',}
+        else:
+            return {'message' : 'Not Authorized, incorrect Password',}
+    else:
+        return{'message' : 'User does not exist',}
+
+def sign_up(username, password, first_name, last_name):
+    user = user_collection.find_one({'Username':username})
+    if user is None:
+        newUser = {
+            "Username": username,
+            "Password": encrypt(password),
+            "First_Name" : first_name,
+            "Last_Name": last_name,
+            "Projects" : [],
+            "HWS1_Checked_Out" : [],
+            "HWS2_Checked_Out" : []
+        }
+        user_collection.insert_one(newUser)
+        return {'message': 'User added.', }
+    else:
+        return {'message': 'Username exists already.', }
+    
+
+# END OF USER VALIDATION
+
+
+
+# PROJECT RELATED FUNCTIONS
+def project_in_user_projects(project_ID, user_projects):
+    for projects in user_projects:
+        user_project_id = str(projects[1])
+        if user_project_id == project_ID:
+            return True
+    return False
+
+
+
+def get_projects(username):
+    user_project = user_collection.find_one({'Username':username}) ['Projects']
+    json_user_projects = json.loads(jsonEncoder.MongoJSONEncoder().encode(user_project))
+    user_projects = []
+
+    for project in json_user_projects:
+        actualProject = project_collection.find_one({'_id':ObjectId(project[1])}) #'_id':ObjectId(project[1] - this is the ID that MongoDB assigs to the project
+        user_projects.append(json.loads(jsonEncoder.MongoJSONEncoder().encode(actualProject)))
+    return 
+
+def join_project(username, project_ID):
+    user_projects = user_collection.find_one({'Username':username})['Projects'] #get the projects array
+    project_joining = project_collection.find_one({'_id':ObjectId(project_ID)}) #gets the project from the projects collection
+    authorized_users = project_joining['Authorized_Users']
+    project_name_id = []
+    project_name_id.append(project_joining['Name'])
+    project_name_id.append(project_joining['_id'])
+
+
+    if username not in authorized_users and username is not None: # if the user exist and is not an authorized user
+        authorized_users.append(username)
+        project_collection.update_one({'_id':ObjectId(project_ID)}, {'$set':{'Authorized_Users': authorized_users}}) #update the document in the Projects Collection
+    if not project_in_user_projects(project_ID, user_projects) and project_joining is not None:
+        user_projects.append(project_name_id)
+        user_collection.update_one({'Username':username}, {'$set':{'Projects':user_projects}})
+
+def join_project_by_id(username, project_ID):
+    if ObjectId.is_valid(project_ID):
+        user_projects = user_collection.find_one({'Username':username})['Projects']
+        project_joining = project_collection.find_one({'_id':ObjectId(project_ID)})
+        authorized_users = project_joining['Authorized_Users']
+
+        if project_joining is not None:
+            if username not in authorized_users  and username is not None:
+                authorized_users.append(username)
+                project_collection.update_one({'_id':ObjectId(project_ID)}, {'$set':{'Authorized_Users':authorized_users }})
+            if not project_in_user_projects(project_ID, user_projects) and project_joining is not None:
+                user_projects.append(project_joining)
+                user_collection.update_one({'Username':username}, {'$set':{'Projects':user_projects}})
+            return {'message':'Project joined.',}
+        else:
+            return {'message':'Project does not exist.',}
+    else:
+        return {'message':'Project ID is invalid.',}
+
+def leave_project(username, project_ID):
+    user_projects = user_collection.find_one({'Username': username})['Projects']
+    project_leaving = project_collection.find_one({'_id':ObjectId(project_ID)})
+    authorized_users = project_leaving['Authorized_Users']
+    project_name_id = []
+    project_name_id.append(project_leaving['Name'])
+    project_name_id.append(project_leaving['_id'])
+
+    if username in authorized_users  and username is not None:
+        authorized_users.remove(username) #remove the username from the autho user of the project
+        project_collection.update_one({'_id':ObjectId(project_ID)}, {'$set':{'Authorized_Users':authorized_user }})
+    if  project_in_user_projects(project_ID, user_projects) and project_leaving is not None:
+        user_projects.remove(project_leaving) #remove the project from the project array of the user
+        user_collection.update_one({'Username':username}, {'$set':{'Projects':user_projects}})
+
+
+
+def create_project(username, project_name, project_description):
+    newProject = {"Name": project_name,
+        "Description": project_description,
+        "Authorized_Users":[username],
+        }
+    project_ID = project_collection.insert_one(newProject).inserted_id
+
+    join_project(username, project_ID)
+    return {'Message': 'Project created.',}
+
+
+
+
+
+
+# END OF PROJECT RELATED FUNCTIONS
+
+
+
+
+# HWSET RELATED FUNCTIONS
+
+# END OF HWSET RELATED FUNCTIONS
--- backend/Server.py
+++ backend/Server.py
@@ -1,16 +1,13 @@
 from pymongo import MongoClient
-import User
-import Project
-import HWSet
-import Driver
+import User, Project, HWSet, Driver, json, Helpers
 from flask import Flask, request, session
-import json
+
 
 app = Flask(__name__)
+#this is for Flask session, so data is unique between users
 app.secret_key = "This Is The Key"
 
-# @app.route("/login")
-# @app.route("/newuser")
+
 user = Driver.Driver()
 set1 = HWSet.HWSet()
 set2 = HWSet.HWSet()
@@ -35,28 +32,27 @@ def addProj(projectID):
 # Not finished, will eventually return integer value of <HWSet> availability
 
 
-@app.route("/checkIn/<projectID>/<HWSet>/<qty>", methods=['GET'])
-def checkIn(projectID, HWSet, qty):
-    try:
-        if user.doesExist(projectID) == "true":
-            newSet = user.getSet(HWSet)
-            if newSet.getName() == "set1":
-                if user.hasEnough(1, qty) == true:
-                    user.setCheckedIn()
+# @app.route("/checkIn/<projectID>/<HWSet>/<qty>", methods=['GET'])
+# def checkIn(projectID, HWSet, qty):
+#     try:
+#         if user.doesExist(projectID) == "true":
+#             newSet = user.getSet(HWSet)
+#             if newSet.getName() == "set1":
+#                 if user.hasEnough(1, qty) == true:
+#                     user.setCheckedIn()
 
 # Not finished, will eventually return integer value of <HWSet> availability
 
 
-@app.route("/checkOut/<projectID>/<HWset>/<qty>", methods=['GET'])
-def checkOut(projectID, HWSet, qty):
-    if user.doesExist(projectID):
-        if set1.getName() == HWSet:
-            if user.getSetOne() - qty > 0:
-                if ()
-                return set1.checkIn(qty)
+# @app.route("/checkOut/<projectID>/<HWset>/<qty>", methods=['GET'])
+# def checkOut(projectID, HWSet, qty):
+#     if user.doesExist(projectID):
+#         if set1.getName() == HWSet:
+#             if user.getSetOne() - qty > 0:
+#                 if ()
+#                 return set1.checkIn(qty)
 
 # Doesn't return anything, will add new user to database
-
 
 @app.route("/createUser/<username>/<password>/<userID>", methods=['GET'])
 def createUser(username, password, userID):
@@ -64,6 +60,89 @@ def createUser(username, password, userID):
     newUser.initialize_user(username, password, userID)
     user.setUser(username, password)
     user.setTrueUser(newUser)
+
+#
+# These function Return JSON
+#
+#
+
+#Route for Login Page
+@app.route("/login", methods=['POST'])
+def log_on():
+    response = ''
+    Username = request.form['Username']
+    Password = request.form['Password']
+    response = User.sign_in(Username, Password)
+    session['username'] = Username
+    return response #This is a Json Response
+
+#Route for Sign Up Page
+@app.route("/create_account", methods=['POST'])
+def create_log_on():
+    response = ''
+    first_name = request.form['First_Name']
+    last_name = request.form['Last_Name']
+    Username = request.form['Username']
+    Password = request.form['Password']
+    response = User.sign_up(Username, Password, first_name, last_name)
+    session['username'] = Username
+    return response #This is a Json Response
+
+@app.route("/my-projects")
+def my_projects():
+    if 'username' in session:
+        username = session['username']
+        projects = User.get_projects(username)
+        return projects #This is a Json Response
+
+    else:
+        return {'status': 'error', 'message': 'Log in or Sign Up',}
+
+
+
+# Join a project means - Adding the Project to the user's project array and adding the user to the Authorized users
+#of said Project
+@app.route("/join-project", methods=['POST'])
+def join_project():
+    response = ''
+    if 'username' in session:
+        username = session['username'] 
+        project_ID = request.form['Project_ID']
+        response = Helpers.joinProject(username, project_ID)
+        return response
+
+    else:
+        return {'status': 'error', 'message': 'Log in or Sign Up',}
+    
+@app.route("/leave-project", methods=['POST'])
+def leave_project ():
+    response = ''
+    if 'username' in session:
+        username = session['username']
+        project_ID = request.form['Project_ID']
+        Helpers.leave_project(username, project_ID)
+        return {'status': 'success',}
+    else:
+        return {'status': 'error', 'message': 'Log in or Sign Up',}
+   
+
+
+@app.route("/create-project", methods=['POST'])
+def create_project():
+    response = ''
+    if 'username' in session:
+        username = session['username'] 
+        project_name = request.form['Project_Name']
+        project_description = request.form['Project_Description'] 
+
+        response = Helpers.create_project(username, project_name, project_description)
+        return response
+    else:
+        return {'status': 'error', 'message': 'Please log in or sign up.',}
+
+
+
+
 
 
 if __name__ == "__main__":
