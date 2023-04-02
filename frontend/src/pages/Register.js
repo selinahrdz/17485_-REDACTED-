ec8e3c5d2959cea1abcaf9e94e730fb4f9503f64
Alexis Torres <alexistorres@utexas.edu>
2023-04-02 15:24:52 -0500

    Remove json file,pycache, etc, reworked login,Reg

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -14,6 +14,10 @@ function Register() {
     pass: "",
   });
 
+  //For Modal Notification
+  const [showNotification, setShowNotification] = useState(false);
+  const [notification, setNotification] = useState("");
+
   function dataToBackend(e) {
     e.preventDefault();
 
@@ -22,16 +26,18 @@ function Register() {
     form.append("Password", Data.pass);
     form.append("UserID", Data.userID);
 
-
     e.target.reset();
 
-    fetch("http://localhost:5000/create_account", { method: "POST", body: form })
+    fetch("http://localhost:5000/create_account", {
+      method: "POST",
+      body: form,
+    })
       .then((response) => response.json())
       .then((data) => {
         if (data["message"] == "User added.") {
           move("/my_projects");
         } else {
-          //Place Holder for Modal
+          setNotification(data["message"]);
         }
         console.log(Data);
       });
@@ -42,53 +48,79 @@ function Register() {
     newData[e.target.id] = e.target.value;
     setData(newData);
   }
+
+  function showNotificationModal() {
+    setShowNotification(!showNotification);
+  }
+
   return (
-    <div className="text-center">
-      <h2 className="text-center">Register</h2>
-      <form className="container m-5" onSubmit={(e) => dataToBackend(e)}>
-        <div className="form-row align-items-center">
-          <div class="form-group col-md-6 mx-sm-3">
-            <label for="inputEmail4">User ID</label>
-            <input
-              type="text"
-              className="form-control mx-sm-3"
-              id="firstname"
-              placeholder="User ID"
-              onChange={(e) => handler(e)}
-            />
+    <>
+      <NotiModal
+        show={showNotification}
+        type="Server Message"
+        noti={notification}
+        handleClose={showNotificationModal}
+      />
+
+      <div className="container mt-5">
+        <div className="row d-flex justify-content-center">
+          <div className="col-md-6">
+            <div className="card px-5 py-5">
+              <h2 className="Auth-form-title text-center">Register</h2>
+              <form className="" onSubmit={(e) => dataToBackend(e)}>
+                <div className="forms-inputs mb-4">
+                  <label className="inputPassword6">User ID</label>
+                  <input
+                    type="text"
+                    className="form-control mx-sm-3"
+                    id="firstname"
+                    placeholder="User ID"
+                    onChange={(e) => handler(e)}
+                    required
+                  />
+                </div>
+                <div className="forms-inputs mb-4">
+                  {" "}
+                  <label>Username</label>
+                  <input
+                    id="user"
+                    onChange={(e) => handler(e)}
+                    type="text"
+                    className="form-control mx-sm-3"
+                    placeholder="username"
+                    required
+                  />
+                </div>
+                <div className="forms-inputs mb-4">
+                  {" "}
+                  <label>Password</label>
+                  <input
+                    id="pass"
+                    onChange={(e) => handler(e)}
+                    type="password"
+                    className="form-control mx-sm-3"
+                    placeholder="password"
+                    required
+                  />
+                </div>
+                <div className="text-center">
+                  {" "}
+                  <button className="btn btn-primary mt-2 " type="submit">
+                    Sign Up
+                  </button>
+                </div>
+              </form>
+              <button
+                className=" btn btn-primary mt-2"
+                onClick={() => move("/login")}
+              >
+                Already have an account? Login Here
+              </button>
+            </div>
           </div>
-          <div className="form-group col-md-6 mx-sm-3">
-            {" "}
-            <label>Username</label>
-            <input
-              id="user"
-              onChange={(e) => handler(e)}
-              type="text"
-              className="form-control mx-sm-3"
-              placeholder="username"
-            />
-          </div>
-          <div className="form-group col-md-6 mx-sm-3">
-            {" "}
-            <label>Password</label>
-            <input
-              id="pass"
-              onChange={(e) => handler(e)}
-              type="password"
-              className="form-control mx-sm-3"
-              placeholder="password"
-            />
-          </div>
-          <button className="btn btn-primary mt-2 " type="submit">
-            Sign Up
-          </button>
         </div>
-      </form>
-      {""}
-      <button className=" btn btn-primary mt-2" onClick={() => move("/login")}>
-        Already have an account? Login Here
-      </button>
-    </div>
+      </div>
+    </>
   );
 }
 
